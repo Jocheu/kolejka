1361c9763f4f1c58d68531092d04f6861a53329e
Dominos0321 <dsch792@gmail.com>
2022-10-28 16:43:28 +0200

    Kolejka functionality added

--- app/src/main/java/com/example/kolejka/MainActivity.kt
+++ app/src/main/java/com/example/kolejka/MainActivity.kt
@@ -7,13 +7,14 @@ import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import java.util.Stack
+import java.util.LinkedList
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         var planets = Stack<String>()
-        planets.addAll(listOf("Mercury"))
+        planets.addAll(listOf("cos1", "cos2", "cos3"))
         findViewById<TextView>(R.id.txt1).text = planets.peek()
         var txtView = findViewById<TextView>(R.id.txt1)
 
@@ -25,7 +26,7 @@ class MainActivity : AppCompatActivity() {
         }
         //usuwanie ze stosu
         findViewById<Button>(R.id.button2).setOnClickListener {
-            Toast.makeText(this, planets.isNotEmpty().toString(), Toast.LENGTH_SHORT).show()
+            //Toast.makeText(this, planets.isNotEmpty().toString(), Toast.LENGTH_SHORT).show()
 
             if(planets.isNotEmpty()){
                 planets.pop()
@@ -39,6 +40,37 @@ class MainActivity : AppCompatActivity() {
             else{
                 txtView.text = ""
             }
+        }
+
+        //kolejka
+        var txtView2 = findViewById<TextView>(R.id.textView)
+        var licznik = findViewById<TextView>(R.id.textView3)
+        var kolejka = LinkedList<String>()
+        kolejka.addAll(listOf("innecos1", "innecos2", "innecos3"))
+        txtView2.text = kolejka.first
+        licznik.text = "Długość kolejki: " + kolejka.size
+        //przesuwanie kolejki
+        findViewById<Button>(R.id.btnAdvance).setOnClickListener {
+            if(kolejka.isNotEmpty()){
+                kolejka.removeFirst()
+                if(kolejka.isEmpty()){
+                    txtView2.text = ""
+                }
+                else{
+                    txtView2.text = kolejka.first
+                }
+            }
+            else{
+                txtView2.text = ""
+            }
+            licznik.text = "Długość kolejki: " + kolejka.size
+        }
+        //dodawanie do kolejki
+        findViewById<Button>(R.id.btnAddToQueue).setOnClickListener {
+            var txtbox = findViewById<EditText>(R.id.editTextTextPersonName2).text
+            kolejka.addLast(txtbox.toString())
+            txtView2.text = kolejka.first
+            licznik.text = "Długość kolejki: " + kolejka.size
         }
     }
 }
